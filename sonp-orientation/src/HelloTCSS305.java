61c3911768488a7c0f8df3232dbf516f00748b5e
sonp <sonp@e0a764ce-2368-11df-b59c-8978f251a94e>
2009-10-08 05:34:07 +0000

    Final version

--- sonp-orientation/src/HelloTCSS305.java
+++ sonp-orientation/src/HelloTCSS305.java
@@ -1,12 +1,15 @@
 /**
+ * 
  * @author Son Pham
- * @version 2
+ * @version 3
  * TCSS 305 - Autumn 2009
  * Assignment 0 - Orientation
  * 7 October 2009
  *
  * This program simply returns the string "Hello World!" to the screen as an output.
+ * 
  */
+
 public final class HelloTCSS305
 {
   /**
@@ -18,10 +21,10 @@ public final class HelloTCSS305
   }
   /**
    * This method simply returns the string "Hello World!" to the screen.
-   * @param args take something from the input
+   * @param the_args take something from the input
    */
-  public static void main(final String[] args)
+  public static void main(final String[] the_args)
   {
     System.out.println("Hello World!");
-  }
+  }  
 }
