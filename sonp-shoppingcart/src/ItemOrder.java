20a7931e34ce339700aff0680408b6fbc0c2a8b8
sonp <sonp@e0a764ce-2368-11df-b59c-8978f251a94e>
2009-10-11 04:16:40 +0000

    First working files.

--- sonp-shoppingcart/src/ItemOrder.java
+++ sonp-shoppingcart/src/ItemOrder.java
@@ -1,19 +1,51 @@
+/**
+ * ItemOrder stores information about a purchase order for a particular item.
+ * @author Son
+ * @version 1.0
+ *
+ */
 public class ItemOrder
 {
+  //Instance fields
+  
+  /**
+   * The item of this order
+   */
+  private final Item my_item;
+  
+  /**
+   * The quantity of this order
+   */
+  private final int my_quantity;
+  
+  //Constructors
+  
+  /**
+   * Constructs an ItemOrder with specified item and quantity.
+   * @param the_item The item which is purchased
+   * @param the_quantity The quantity which is purchased
+   */
   public ItemOrder(final Item the_item, final int the_quantity)
   {
-    assert false;
+    my_item = the_item;
+    my_quantity = the_quantity;
   }
+  
+  //Instance methods
 
+  /**
+   * @return the cost for this item order
+   */
   public double getPrice()
   {
-    assert false;
-    return 0.0;
+    return my_item.priceFor(my_quantity);
   }
 
+  /**
+   * @return a reference to the item of this order 
+   */
   public Item getItem()
   {
-    assert false;
-    return null;
+    return my_item;
   }
 }
--- sonp-shoppingcart/src/ShoppingCart.java
+++ sonp-shoppingcart/src/ShoppingCart.java
@@ -1,23 +1,50 @@
+import java.util.HashMap;
+import java.util.Iterator;
+
+/**
+ * ShoppingCart stores information about customer's overall purchase.
+ * @author Son
+ * @version 1.0
+ *
+ */
 public class ShoppingCart
 {
+  private HashMap<Item, Double> my_hashmap; 
+  private boolean my_discount;
+  
   public ShoppingCart()
   {
-    assert false;
+    my_hashmap = new HashMap<Item, Double>();
   }
 
   public void add(final ItemOrder the_order)
   {
-    assert false;
+    Item the_order_item = the_order.getItem();
+    double the_order_price = the_order.getPrice();
+    
+    my_hashmap.put(the_order_item, the_order_price); 
   }
 
   public void setDiscount(final boolean the_discount)
   {
-    assert false;
+    my_discount = the_discount;
   }
 
   public double getTotal()
   {
-    assert false;
-    return 0.0;
+    double the_undiscount_price = 0.0;
+    Iterator<Item> the_iterator = my_hashmap.keySet().iterator();
+    while (the_iterator.hasNext())
+    {
+      Item the_current_item = the_iterator.next();
+      double the_current_order_price = my_hashmap.get(the_current_item);
+      
+      the_undiscount_price += the_current_order_price;
+    }
+    if (my_discount)
+    {
+      return 0.9 * the_undiscount_price;
+    }
+    else return the_undiscount_price;
   }
 }
