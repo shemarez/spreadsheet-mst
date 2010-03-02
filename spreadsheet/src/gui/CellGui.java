31a2bd4cbee704d82d360f1beb3a1f0c2515d751
parabolster <parabolster@e0a764ce-2368-11df-b59c-8978f251a94e>
2010-03-02 08:53:54 +0000

    alternate gui with JTable and binding

---
+++ spreadsheet/src/gui/CellGui.java
@@ -0,0 +1,56 @@
+package gui;
+
+import javax.swing.JTextField;
+
+import tokens.CellToken;
+
+/**
+ * CellGui is a text field that hold cell token reference to a spreadsheet.
+ * 
+ * @author Son Pham
+ * @version 1.0
+ */
+@SuppressWarnings("serial")
+public class CellGui extends JTextField {
+	/**
+	 * The cell token of this object.
+	 */
+	private CellToken my_cell_token;
+
+	// Constructors
+
+	/**
+	 * Construct a default CellGui.
+	 */
+	public CellGui() {
+		my_cell_token = new CellToken(0, 0);
+	}
+
+	/**
+	 * Construct a CellGui with the input cell token.
+	 * 
+	 * @param the_token
+	 *            The cell token.
+	 */
+	public CellGui(final CellToken the_token) {
+		my_cell_token = the_token;
+	}
+
+	/**
+	 * 
+	 * @return The cell token of this CellGui.
+	 */
+	public CellToken getCellToken() {
+		return my_cell_token;
+	}
+
+	/**
+	 * Set the cell token of this CellGui.
+	 * 
+	 * @param the_token
+	 *            The cell token.
+	 */
+	public void setCellToken(final CellToken the_token) {
+		my_cell_token = the_token;
+	}
+}
---
+++ spreadsheet/src/tokens/CellToken.java
@@ -0,0 +1,88 @@
+package tokens;
+
+/**
+ * Cell token for the spreadsheet.
+ * @author Son Pham.
+ * @version 1.0
+ */
+public class CellToken extends Token
+{
+  /**
+   * The column of this cell in the spreadsheet.
+   */
+  private int my_column;
+  
+  /**
+   * The row of this cell in the spreadsheet.
+   */
+  private int my_row;
+  
+  //Constructors
+  
+  /**
+   * Construct an default cell with the row and column are both 0. 
+   */
+  public CellToken()
+  {
+    my_column = 0;
+    my_row = 0;
+  }
+  
+  /**
+   * Construct a cell with number of rows and columns from the inputs. 
+   */
+  public CellToken(final int the_rows, int the_columns)
+  {
+    my_row = the_rows;
+    my_column = the_columns;    
+  }
+  
+  //Instance methods
+  
+  /**
+   * @return The column of this cell in the spreadsheet.
+   */
+  public int getColumn()
+  {
+    return my_column;
+  }
+  
+  /**
+   * @return The row of this cell in the spreadsheet.
+   */
+  public int getRow()
+  {
+    return my_row;
+  }
+  
+  /**
+   * Set the column of this cell.
+   */
+  public void setColumn(final int col)
+  {
+    my_column = col;
+  }
+  
+  /**
+   * Set the row of this cell.
+   */
+  public void setRow(final int row)
+  {
+    my_row = row;
+  }
+  
+  public String columnToString(){
+	  StringBuffer sb = new StringBuffer();
+	  int colDiv = my_column / 26;
+	  int colRem = my_column % 26;
+	  char ch = (char) (colRem + 'A');
+	  sb.append(ch);
+	  while ( colDiv > 0 ){
+		  colRem = (colDiv - 1) % 26;
+		  colDiv = colDiv / 26;
+		  ch = (char) (colRem + 'A');
+		  sb.insert(0, ch);
+	  }
+	  return sb.toString();
+  }
+}
