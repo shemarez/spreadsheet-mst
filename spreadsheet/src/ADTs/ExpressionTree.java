19f6d66da3ed146c256192d31e003800ce23a307
tdoll.uwt <tdoll.uwt@e0a764ce-2368-11df-b59c-8978f251a94e>
2010-03-03 04:51:05 +0000

    Added exponents and unary minus functionality

--- spreadsheet/src/ADTs/ExpressionTree.java
+++ spreadsheet/src/ADTs/ExpressionTree.java
@@ -12,107 +12,118 @@ import tokens.Token;
  * @author Teddy Doll
  * @version 1.0
  */
-public class ExpressionTree
-{
-  //Instance field
+public class ExpressionTree {
+	// Instance field
 
-  /**
-   * The root of this tree.
-   */
-  ExpressionTreeNode root;
+	/**
+	 * The root of this tree.
+	 */
+	ExpressionTreeNode root;
 
-  //Constructor
+	// Constructor
 
-  /**
-   * Construct an expression tree contains only its root.
-   * @param root The root.
-   */
-  public ExpressionTree(ExpressionTreeNode root)
-  {
-    this.root = root;
-  }
+	/**
+	 * Construct an expression tree contains only its root.
+	 * 
+	 * @param root
+	 *            The root.
+	 */
+	public ExpressionTree(ExpressionTreeNode root) {
+		this.root = root;
+	}
 
-  //Instance methods
-  
-  /**
-   * Make the tree empty.
-   */
-  public void makeEmpty() {
-    this.root = null;
-  }
+	// Instance methods
 
-  /**
-   * Print the tree to the console.
-   */
-  public void printTree() {
-    ;
-  }
+	/**
+	 * Make the tree empty.
+	 */
+	public void makeEmpty() {
+		this.root = null;
+	}
 
-  /**
-   * 
-   * @return The root of this tree.
-   */
-  public ExpressionTreeNode getRoot() {
-    return this.root;
-  }
+	/**
+	 * Print the tree to the console.
+	 */
+	public void printTree() {
+		;
+	}
 
-  /**
-   * Set the root of this tree to be the new root.
-   * @param the_root The root.
-   */
-  public void setRoot(ExpressionTreeNode the_root) {
-    this.root = the_root;
-  }
+	/**
+	 * 
+	 * @return The root of this tree.
+	 */
+	public ExpressionTreeNode getRoot() {
+		return this.root;
+	}
 
-  /**
-   * 
-   * @param spreadsheet The spreadsheet.
-   * @return The value of this tree.
-   */
-  public int Evaluate(Spreadsheet spreadsheet) {
-    return evaluteTree(spreadsheet, this.root);
-  }
+	/**
+	 * Set the root of this tree to be the new root.
+	 * 
+	 * @param the_root
+	 *            The root.
+	 */
+	public void setRoot(ExpressionTreeNode the_root) {
+		this.root = the_root;
+	}
 
-  /**
-   * 
-   * @param spreadsheet The spreadsheet.
-   * @param the_root The root.
-   * @return The value of this tree whose root is the_root.
-   */
-  private int evaluteTree(Spreadsheet spreadsheet, ExpressionTreeNode the_root) {
-    if (the_root == null)
-      return 0;
-    Token rootToken = (Token) the_root.getToken();
-    if (rootToken instanceof LiteralToken) {
-      return ((LiteralToken) the_root.getToken()).getValue();
-    } else if (rootToken instanceof CellToken) {
-      return spreadsheet.getCellValue((CellToken) rootToken);
-    } else {
-      int leftTreeValue;
-      int rightTreeValue;
-      int returnValue = 0;
-      leftTreeValue = evaluteTree(spreadsheet, the_root.getLeft());
-      rightTreeValue = evaluteTree(spreadsheet, the_root.getRight());
-      switch (((OperatorToken) rootToken).getOperatorToken()) {
-      case OperatorToken.PLUS:
-	returnValue = leftTreeValue + rightTreeValue;
-	break;
-      case OperatorToken.MINUS:
-	returnValue = leftTreeValue - rightTreeValue;
-	break;
-      case OperatorToken.MULT:
-	returnValue = leftTreeValue * rightTreeValue;
-	break;
-      case OperatorToken.DIV:
-	returnValue = leftTreeValue / rightTreeValue;
-	break;
-      default:
-	// Should never happen
-	System.out.println("error in tree eval");
-	break;
-      }
-      return returnValue;
-    }
-  }
+	/**
+	 * 
+	 * @param spreadsheet
+	 *            The spreadsheet.
+	 * @return The value of this tree.
+	 */
+	public int Evaluate(Spreadsheet spreadsheet) {
+		return evaluteTree(spreadsheet, this.root);
+	}
+
+	/**
+	 * Recursively evaluate this ExpressionTree and return the value. 
+	 * @param spreadsheet
+	 *            The spreadsheet.
+	 * @param the_root
+	 *            The root.
+	 * @return The value of this tree whose root is the_root.
+	 */
+	private int evaluteTree(Spreadsheet spreadsheet, ExpressionTreeNode the_root) {
+		if (the_root == null)
+			return 0;
+		Token rootToken = (Token) the_root.getToken();
+		if (rootToken instanceof LiteralToken) {
+			return ((LiteralToken) the_root.getToken()).getValue();
+		} else if (rootToken instanceof CellToken) {
+			return spreadsheet.getCellValue((CellToken) rootToken);
+		} else {
+			int leftTreeValue;
+			int rightTreeValue;
+			int returnValue = 0;
+			leftTreeValue = evaluteTree(spreadsheet, the_root.getLeft());
+			rightTreeValue = evaluteTree(spreadsheet, the_root.getRight());
+			switch (((OperatorToken) rootToken).getOperatorToken()) {
+			case OperatorToken.PLUS:
+				returnValue = leftTreeValue + rightTreeValue;
+				break;
+			case OperatorToken.MINUS:
+				returnValue = leftTreeValue - rightTreeValue;
+				break;
+			case OperatorToken.MULT:
+				returnValue = leftTreeValue * rightTreeValue;
+				break;
+			case OperatorToken.DIV:
+				returnValue = leftTreeValue / rightTreeValue;
+				break;
+			case OperatorToken.EXP:
+				returnValue = (int) Math.pow(leftTreeValue, rightTreeValue);
+				break;
+			case OperatorToken.U_MINUS:
+				returnValue = rightTreeValue * -1;
+				break;
+			default:
+				// Should never happen
+				System.out.println("error in tree eval");
+				break;
+			}
+			return returnValue;
+		}
+	}
 
 }
--- spreadsheet/src/tokens/OperatorToken.java
+++ spreadsheet/src/tokens/OperatorToken.java
@@ -2,127 +2,146 @@ package tokens;
 
 /**
  * Operator token for the spreadsheet.
+ * 
  * @author Son Pham
  * @version 1.0
  */
-public class OperatorToken extends Token
-{
-  /**
-   * Plus character.
-   */
-  public static final char PLUS = '+';
-  
-  /**
-   * Minus character.
-   */
-  public static final char MINUS = '-';
-  
-  /**
-   * Multiply character.
-   */
-  public static final char MULT = '*';
-  
-  /**
-   * Divide character.
-   */
-  public static final char DIV = '/';
-  
-  /**
-   * Left parenthesis character.
-   */
-  public static final char LEFT_PAREN = '(';
-  
-  /**
-   * The character of this operator token.
-   */
-  private char my_operator_token;
-  
-  //Constructor
-  
-  /**
-   * Construct an operator token base on the input token.
-   * @param token The token.
-   */
-  public OperatorToken (final char token)
-  {
-    if (isOperator(token))
-    {
-      switch (token)
-      {
-        case PLUS:
-  	my_operator_token = PLUS;
-  	break;
-        case MINUS:
-  	my_operator_token = MINUS;
-  	break;
-        case MULT:
-  	my_operator_token = MULT;
-  	break;
-        case DIV:
-  	my_operator_token = DIV;
-  	break;
-        case LEFT_PAREN:
-  	my_operator_token = LEFT_PAREN;
-  	break;
-      }
-    }
-  }
-  
-  /**
-   * Return true if the char ch is an operator of a formula.
-   * Current operators are: +, -, *, /, (.
-   * @param ch a char
-   * @return whether ch is an operator
-   */
-  public static boolean isOperator (char ch) 
-  {
-      return ((ch == PLUS) ||
-              (ch == MINUS) ||
-              (ch == MULT) ||
-              (ch == DIV) ||
-              (ch == LEFT_PAREN) );
-  }
-  
-  /**
-   * Return the priority of this OperatorToken.
-   * <p>
-   * priorities:
-   * <br>  +, - : 0
-   * <br>  *, / : 1
-   * <br>  (    : 2
-   *
-   * @return  the priority of operatorToken
-   */
-  public int priority() 
-  {
-    int priority = 0;
-    switch (my_operator_token) 
-    {
-      case PLUS:
-        priority = 0; break;
-      case MINUS:
-        priority = 0; break;
-      case MULT:
-        priority = 1; break;
-      case DIV:
-        priority = 1; break;
-      case LEFT_PAREN:
-        priority = 2; break;
-  
-      default:
-        // This case should NEVER happen
-        System.out.println("Error in priority.");
-        System.exit(0);
-        break;
-    }
-    return priority;
-  }
-  
-  /**
-   * @return The character of this operator.
-   */
-  public char getOperatorToken()
-  {
-    return my_operator_token;
-  }
+public class OperatorToken extends Token {
+	/**
+	 * Plus character.
+	 */
+	public static final char PLUS = '+';
+
+	/**
+	 * Minus character.
+	 */
+	public static final char MINUS = '-';
+
+	/**
+	 * Multiply character.
+	 */
+	public static final char MULT = '*';
+
+	/**
+	 * Divide character.
+	 */
+	public static final char DIV = '/';
+
+	/**
+	 * Exponent character.
+	 */
+	public static final char EXP = '^';
+	/**
+	 * Left parenthesis character.
+	 */
+	public static final char LEFT_PAREN = '(';
+
+	/**
+	 * Unary Minus character.
+	 */
+	public static final char U_MINUS = '~';
+	/**
+	 * The character of this operator token.
+	 */
+	private char my_operator_token;
+
+	// Constructor
+
+	/**
+	 * Construct an operator token base on the input token.
+	 * 
+	 * @param token
+	 *            The token.
+	 */
+	public OperatorToken(final char token) {
+		if (isOperator(token)) {
+			switch (token) {
+			case PLUS:
+				my_operator_token = PLUS;
+				break;
+			case MINUS:
+				my_operator_token = MINUS;
+				break;
+			case MULT:
+				my_operator_token = MULT;
+				break;
+			case DIV:
+				my_operator_token = DIV;
+				break;
+			case EXP:
+				my_operator_token = EXP;
+				break;
+			case LEFT_PAREN:
+				my_operator_token = LEFT_PAREN;
+				break;
+			case U_MINUS:
+				my_operator_token = U_MINUS;
+				break;
+			}
+		}
+	}
+
+	/**
+	 * Return true if the char ch is an operator of a formula. Current operators
+	 * are: +, -, *, /, (.
+	 * 
+	 * @param ch
+	 *            a char
+	 * @return whether ch is an operator
+	 */
+	public static boolean isOperator(char ch) {
+		return ((ch == PLUS) || (ch == MINUS) || (ch == MULT) || 
+				(ch == DIV) || (ch == EXP) || 
+				(ch == LEFT_PAREN) || (ch == U_MINUS));
+	}
+
+	/**
+	 * Return the priority of this OperatorToken.
+	 * <p>
+	 * priorities: <br>
+	 * +, - : 0 <br>
+	 * *, / : 1 <br>
+	 * ( : 2
+	 * 
+	 * @return the priority of operatorToken
+	 */
+	public int priority() {
+		int priority = 0;
+		switch (my_operator_token) {
+		case PLUS:
+			priority = 0;
+			break;
+		case MINUS:
+			priority = 0;
+			break;
+		case MULT:
+			priority = 1;
+			break;
+		case DIV:
+			priority = 1;
+			break;
+		case EXP:
+			priority = 2;
+		case LEFT_PAREN:
+			priority = 4;
+			break;
+		case U_MINUS:
+			priority = 3;
+			break;
+
+		default:
+			// This case should NEVER happen
+			System.out.println("Error in priority.");
+			System.exit(0);
+			break;
+		}
+		return priority;
+	}
+
+	/**
+	 * @return The character of this operator.
+	 */
+	public char getOperatorToken() {
+		return my_operator_token;
+	}
 }
