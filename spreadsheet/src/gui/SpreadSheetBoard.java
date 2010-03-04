26d3aa12003a713f82b6ca65aa1eea255c1b8da1
tdoll.uwt <tdoll.uwt@e0a764ce-2368-11df-b59c-8978f251a94e>
2010-03-04 06:18:47 +0000

    Miles' Update to GUI

--- spreadsheet/src/gui/SpreadSheetBoard.java
+++ spreadsheet/src/gui/SpreadSheetBoard.java
@@ -9,10 +9,16 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.StringTokenizer;
 
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 import spreadsheet.CycleFound;
 import spreadsheet.Spreadsheet;
@@ -24,18 +30,103 @@ import tokens.CellToken;
  * 
  * @author Son Pham
  * @version 1.0
+ * 
+ *          2010-03-03 Miles
  */
 @SuppressWarnings("serial")
 public class SpreadSheetBoard extends JPanel {
-	// Instance fields
-	public static final int DEFAULT_CELL_WIDTH = 80;
+	/**
+	 * Focus listener for a cell GUI
+	 * 
+	 * @author Son Pham
+	 * @version 1.0
+	 */
+	private class CellFocusListener extends FocusAdapter {
+		/**
+		 * Show the value of the cell when losing focus.
+		 */
+		@Override
+		public void focusLost(FocusEvent the_event) {
+			final CellGui cell_gui = (CellGui) the_event.getComponent();
+			// cell_gui.setText(my_spreadsheet.cellValueToString(cell_gui.getCellToken()));
+			setCellText(cell_gui);
+		}
+
+	}
+
+	/**
+	 * Keyboard listener class for a cell GUI
+	 * 
+	 * @author Son Pham
+	 * @version 1.0
+	 */
+	private class CellKeyListener extends KeyAdapter {
+		/**
+		 * Press enter to commit change to the cell.
+		 */
+		@Override
+		public void keyPressed(KeyEvent the_event) {
+			if (the_event.getKeyCode() == KeyEvent.VK_ENTER) {
+				final CellGui cell_gui = (CellGui) the_event.getComponent();
+				try {
+					my_spreadsheet.changeCellFormulaAndRecalculate(cell_gui
+							.getCellToken(), cell_gui.getText());
+					cell_gui.setText(my_spreadsheet.cellValueToString(cell_gui
+							.getCellToken()));
+				} catch (CycleFound e) {
+					JOptionPane.showMessageDialog(null,
+							"Cycle Found! Please re-enter formula",
+							"Cycle error", JOptionPane.ERROR_MESSAGE);
+					my_spreadsheet.revert(cell_gui.getCellToken());
+				} catch (ArrayIndexOutOfBoundsException e) {
+					JOptionPane
+							.showMessageDialog(
+									null,
+									"Cell's index out of bound! Please re-enter formula",
+									"Cell's out of bound",
+									JOptionPane.ERROR_MESSAGE);
+					my_spreadsheet.revert(cell_gui.getCellToken());
+				} catch (UnpairedParenthesesException e) {
+					JOptionPane.showMessageDialog(null,
+							"Error in formula: Parentheses do not match! \n"
+									+ "Please re-enter formula",
+							"Formula Error", JOptionPane.ERROR_MESSAGE);
+					my_spreadsheet.revert(cell_gui.getCellToken());
+				} catch (ArithmeticException e) {
+					JOptionPane.showMessageDialog(null, e.getMessage(),
+							"Arithmetic Error", JOptionPane.ERROR_MESSAGE);
+					my_spreadsheet.revert(cell_gui.getCellToken());
+				}
+				update();
+			}
+		}
+	}
+
+	/**
+	 * Mouse listener class for a cell GUI.
+	 * 
+	 * @author Son Pham
+	 * @version 1.0
+	 */
+	private class CellMouseListener extends MouseAdapter {
+		/**
+		 * Double click to edit the formula of the cell.
+		 */
+		@Override
+		public void mouseClicked(MouseEvent the_event) {
+			if (the_event.getClickCount() == 2) {
+				final CellGui cell_gui = (CellGui) the_event.getComponent();
+				cell_gui.setText(my_spreadsheet.cellFormulaToString(cell_gui
+						.getCellToken()));
+				cell_gui.setCaretPosition(cell_gui.getDocument().getLength());
+			}
+		}
+	}
 
 	public static final int DEFAULT_CELL_HEIGHT = 30;
 
-	/**
-	 * The spreadsheet.
-	 */
-	private Spreadsheet my_spreadsheet;
+	// Instance fields
+	public static final int DEFAULT_CELL_WIDTH = 80;
 
 	/**
 	 * The cell array of this spreadsheet.
@@ -43,9 +134,16 @@ public class SpreadSheetBoard extends JPanel {
 	private CellGui[][] my_cell_array;
 
 	/**
-	 * The rows in this spreadsheet.
+	 * The cell's height.
 	 */
-	private int my_rows;
+	private int my_cell_height;
+
+	/**
+	 * The cell's width.
+	 */
+	private int my_cell_width;
+
+	// Constructor
 
 	/**
 	 * The columns in this spreadsheet.
@@ -53,16 +151,14 @@ public class SpreadSheetBoard extends JPanel {
 	private int my_columns;
 
 	/**
-	 * The cell's width.
+	 * The rows in this spreadsheet.
 	 */
-	private int my_cell_width;
+	private int my_rows;
 
 	/**
-	 * The cell's height.
+	 * The spreadsheet.
 	 */
-	private int my_cell_height;
-
-	// Constructor
+	private Spreadsheet my_spreadsheet;
 
 	/**
 	 * Construct a spreadsheet board from the input.
@@ -86,6 +182,14 @@ public class SpreadSheetBoard extends JPanel {
 
 	/**
 	 * 
+	 * @return The cell's height.
+	 */
+	public int getCellHeight() {
+		return my_cell_height;
+	}
+
+	/**
+	 * 
 	 * @return The cell's width.
 	 */
 	public int getCellWidth() {
@@ -93,11 +197,100 @@ public class SpreadSheetBoard extends JPanel {
 	}
 
 	/**
-	 * 
-	 * @return The cell's height.
+	 * Initialize the cell array and add the cells into the panel.
 	 */
-	public int getCellHeight() {
-		return my_cell_height;
+	private void init() {
+		for (int i = -1; i < my_rows; i++)
+			for (int j = -1; j < my_columns; j++) {
+				CellToken cellToken = new CellToken(i, j);
+
+				if (i == -1) {
+					if (j == -1)
+						add(new JLabel("", null, SwingConstants.CENTER));
+					else
+						add(new JLabel(cellToken.columnToString(), null,
+								SwingConstants.CENTER));
+				} else if (j == -1)
+					add(new JLabel(Integer.toString(i), null,
+							SwingConstants.CENTER));
+				else {
+					my_cell_array[i][j] = new CellGui(cellToken);
+					// my_cell_array[i][j].setText(my_spreadsheet.cellValueToString(my_cell_array[i][j].getCellToken()));
+					setCellText(my_cell_array[i][j]);
+					my_cell_array[i][j]
+							.addMouseListener(new CellMouseListener());
+					my_cell_array[i][j].addKeyListener(new CellKeyListener());
+					my_cell_array[i][j]
+							.addFocusListener(new CellFocusListener());
+					// my_cell_array[i][j].setPreferredSize(new
+					// Dimension(DEFAULT_CELL_WIDTH, DEFAULT_CELL_HEIGHT));
+					add(my_cell_array[i][j]);
+				}
+			}
+	}
+
+	/**
+	 * Load from a file
+	 * @param canonicalPath
+	 */
+	public void load(String canonicalPath) throws Exception {
+		BufferedReader in = new BufferedReader(new FileReader(canonicalPath));
+		String line = in.readLine();
+		StringTokenizer st;
+		int lineNum = 0, tokenNum = 0;
+		if (line != null)
+			reset();
+		do {
+			tokenNum = 0;
+			st = new StringTokenizer(line, ",");
+			while(st.hasMoreTokens()) {
+				my_spreadsheet.changeCellFormulaAndRecalculate(my_cell_array[lineNum][tokenNum].getCellToken(), st.nextToken());
+				tokenNum++;
+			}
+			lineNum++;
+		} while ((line = in.readLine()) != null);
+		in.close();
+	}
+
+	@Override
+	public void paintComponent(final Graphics the_graphics) {
+		// super.paintComponent(the_graphics);
+		setPreferredSize(new Dimension(my_columns * my_cell_width, my_rows
+				* my_cell_height));
+	}
+
+	/**
+	 * Reset the board. All cells will have value of 0 and blank formulas.
+	 */
+	public void reset() {
+		my_spreadsheet = new Spreadsheet(my_rows, my_columns);
+	}
+
+	/**
+	 * Set the cell's height.
+	 * 
+	 * @param the_height
+	 *            The cell's height.
+	 */
+	public void setCellHeight(final int the_height) {
+		my_cell_height = the_height;
+		setPreferredSize(new Dimension(my_columns * my_cell_width, my_rows
+				* my_cell_height));
+	}
+
+	/**
+	 * Set the text output of the cell. If the cell's formula is not empty, the
+	 * cell will show its value. Otherwise, the cell will be blank.
+	 * 
+	 * @param the_cell
+	 */
+	private void setCellText(final CellGui the_cell) {
+		if (my_spreadsheet.cellFormulaToString(the_cell.getCellToken())
+				.isEmpty())
+			the_cell.setText("");
+		else
+			the_cell.setText(my_spreadsheet.cellValueToString(the_cell
+					.getCellToken()));
 	}
 
 	/**
@@ -113,172 +306,29 @@ public class SpreadSheetBoard extends JPanel {
 	}
 
 	/**
-	 * Set the cell's height.
-	 * 
-	 * @param the_height
-	 *            The cell's height.
-	 */
-	public void setCellHeight(final int the_height) {
-		my_cell_height = the_height;
-		setPreferredSize(new Dimension(my_columns * my_cell_width, my_rows
-				* my_cell_height));
-	}
-
-	public void paintComponent(final Graphics the_graphics) {
-		// super.paintComponent(the_graphics);
-		setPreferredSize(new Dimension(my_columns * my_cell_width, my_rows
-				* my_cell_height));
-	}
-
-	/**
-	 * Reset the board. All cells will have value of 0 and blank formulas.
-	 */
-	public void reset() {
-		my_spreadsheet = new Spreadsheet(my_rows, my_columns);
-	}
-
-	/**
 	 * Update the cells' values of this spreadsheet.
 	 */
 	public void update() {
-		for (int i = 0; i < my_rows; i++) {
-			for (int j = 0; j < my_columns; j++) {
+		for (int i = 0; i < my_rows; i++)
+			for (int j = 0; j < my_columns; j++)
 				setCellText(my_cell_array[i][j]);
+	}
+
+
+	/**
+	 * Save to a file
+	 * @param canonicalPath
+	 */
+	public void save(String canonicalPath) throws Exception {
+		BufferedWriter out = new BufferedWriter(new FileWriter(canonicalPath));
+		for(CellGui[] cells : my_cell_array) {
+			for(CellGui cell : cells) {
+				out.write(my_spreadsheet.cellFormulaToString(cell.getCellToken()));
+				out.append(',');
 			}
+			out.newLine();
+			out.flush();
 		}
-	}
-
-	/**
-	 * Set the text output of the cell. If the cell's formula is not empty, the
-	 * cell will show its value. Otherwise, the cell will be blank.
-	 * 
-	 * @param the_cell
-	 */
-	private void setCellText(final CellGui the_cell) {
-		if (my_spreadsheet.cellFormulaToString(the_cell.getCellToken())
-				.isEmpty()) {
-			the_cell.setText("");
-		} else {
-			the_cell.setText(my_spreadsheet.cellValueToString(the_cell
-					.getCellToken()));
-		}
-	}
-
-	/**
-	 * Initialize the cell array and add the cells into the panel.
-	 */
-	private void init() {
-		for (int i = -1; i < my_rows; i++) {
-			for (int j = -1; j < my_columns; j++) {
-				CellToken cellToken = new CellToken(i, j);
-
-				if (i == -1) {
-					if (j == -1)
-						add(new JLabel("", null, JLabel.CENTER));
-					else
-						add(new JLabel(cellToken.columnToString(), null,
-								JLabel.CENTER));
-				} else if (j == -1) {
-					add(new JLabel(Integer.toString(i), null, JLabel.CENTER));
-				} else {
-					my_cell_array[i][j] = new CellGui(cellToken);
-					// my_cell_array[i][j].setText(my_spreadsheet.cellValueToString(my_cell_array[i][j].getCellToken()));
-					setCellText(my_cell_array[i][j]);
-					my_cell_array[i][j]
-							.addMouseListener(new CellMouseListener());
-					my_cell_array[i][j].addKeyListener(new CellKeyListener());
-					my_cell_array[i][j]
-							.addFocusListener(new CellFocusListener());
-					// my_cell_array[i][j].setPreferredSize(new
-					// Dimension(DEFAULT_CELL_WIDTH, DEFAULT_CELL_HEIGHT));
-					add(my_cell_array[i][j]);
-				}
-			}
-		}
-	}
-
-	/**
-	 * Mouse listener class for a cell GUI.
-	 * 
-	 * @author Son Pham
-	 * @version 1.0
-	 */
-	private class CellMouseListener extends MouseAdapter {
-		/**
-		 * Double click to edit the formula of the cell.
-		 */
-		public void mouseClicked(MouseEvent the_event) {
-			if (the_event.getClickCount() == 2) {
-				final CellGui cell_gui = (CellGui) the_event.getComponent();
-				cell_gui.setText(my_spreadsheet.cellFormulaToString(cell_gui
-						.getCellToken()));
-				cell_gui.setCaretPosition(cell_gui.getDocument().getLength());
-			}
-		}
-	}
-
-	/**
-	 * Keyboard listener class for a cell GUI
-	 * 
-	 * @author Son Pham
-	 * @version 1.0
-	 */
-	private class CellKeyListener extends KeyAdapter {
-		/**
-		 * Press enter to commit change to the cell.
-		 */
-		public void keyPressed(KeyEvent the_event) {
-			if (the_event.getKeyCode() == KeyEvent.VK_ENTER) {
-				final CellGui cell_gui = (CellGui) the_event.getComponent();
-				try {
-					my_spreadsheet.changeCellFormulaAndRecalculate(cell_gui
-							.getCellToken(), cell_gui.getText());
-					cell_gui.setText(my_spreadsheet.cellValueToString(cell_gui
-							.getCellToken()));
-				} catch (CycleFound e) {
-					JOptionPane.showMessageDialog(null,
-							"Cycle Found! Please re-enter formula",
-							"Cycle error", JOptionPane.ERROR_MESSAGE);
-					my_spreadsheet.revert(cell_gui.getCellToken());
-				} catch (ArrayIndexOutOfBoundsException e) {
-					JOptionPane.showMessageDialog(
-									null,
-									"Cell's index out of bound! Please re-enter formula",
-									"Cell's out of bound",
-									JOptionPane.ERROR_MESSAGE);
-					my_spreadsheet.revert(cell_gui.getCellToken());
-				} catch (UnpairedParenthesesException e) {
-					JOptionPane.showMessageDialog(null,
-							"Error in formula: Parentheses do not match! \n"+
-							"Please re-enter formula",
-							"Formula Error", JOptionPane.ERROR_MESSAGE);
-					my_spreadsheet.revert(cell_gui.getCellToken());
-				} catch (ArithmeticException e) {
-					JOptionPane.showMessageDialog(null,
-							e.getMessage(),
-							"Arithmetic Error", JOptionPane.ERROR_MESSAGE);
-					my_spreadsheet.revert(cell_gui.getCellToken());
-				} 
-				update();
-			}
-		}
-	}
-
-	/**
-	 * Focus listener for a cell GUI
-	 * 
-	 * @author Son Pham
-	 * @version 1.0
-	 */
-	private class CellFocusListener extends FocusAdapter {
-		/**
-		 * Show the value of the cell when losing focus.
-		 */
-		public void focusLost(FocusEvent the_event) {
-			final CellGui cell_gui = (CellGui) the_event.getComponent();
-			// cell_gui.setText(my_spreadsheet.cellValueToString(cell_gui.getCellToken()));
-			setCellText(cell_gui);
-		}
-
+		out.close();
 	}
 }
--- spreadsheet/src/gui/SpreadSheetGui.java
+++ spreadsheet/src/gui/SpreadSheetGui.java
@@ -1,9 +1,9 @@
 package gui;
 
 import java.awt.event.ActionEvent;
-
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -25,16 +25,28 @@ public class SpreadSheetGui extends JFrame {
 	 */
 	public static final String FRAME_NAME = "TCSS-342 Simple SpreadSheet";
 
+	public static final int NUM_COLS = 12;
+
 	public static final int NUM_ROWS = 10;
 
-	public static final int NUM_COLS = 12;
+	/**
+	 * The main method of SpreadSheetGui.
+	 * 
+	 * @param the_args
+	 *            The command line argument (ignored).
+	 */
+	public static void main(final String... the_args) {
+		final SpreadSheetGui the_frame = new SpreadSheetGui(new Spreadsheet(
+				NUM_ROWS, NUM_COLS));
+		the_frame.setUp();
+	}
+
+	// Constructor
 
 	/**
 	 * The cell table in the spreadsheet GUI.
 	 */
 	private SpreadSheetBoard my_board;
-
-	// Constructor
 
 	/**
 	 * Construct a SpreadSheetGui.
@@ -48,39 +60,54 @@ public class SpreadSheetGui extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
-	/**
-	 * Setup the GUI.
-	 */
-	public void setUp() {
-		add(my_board);
-		setJMenuBar(menuBar());
-		pack();
-		setVisible(true);
-	}
-
-	/**
-	 * @return The menu bar.
-	 */
-	private JMenuBar menuBar() {
-		final JMenuBar menu_bar = new JMenuBar();
-		menu_bar.add(fileMenu());
-		//menu_bar.add(editMenu());
-		menu_bar.add(helpMenu());
-		return menu_bar;
+	@SuppressWarnings("unused")
+	private JMenu editMenu() {
+		final JMenu edit_menu = new JMenu("Edit");
+		edit_menu.setMnemonic('E');
+		return edit_menu;
 	}
 
 	private JMenu fileMenu() {
 		final JMenu file_menu = new JMenu("File");
 		file_menu.setMnemonic('F');
-		final Action new_file = new AbstractAction("New") {
+		final JMenuItem reset = new JMenuItem(new AbstractAction("New") {
+			@Override
 			public void actionPerformed(final ActionEvent the_event) {
 				my_board.reset();
 				my_board.update();
 			}
-		};
-		final JMenuItem reset = new JMenuItem(new_file);
+		});
 		reset.setMnemonic('N');
 		file_menu.add(reset);
+		final JMenuItem open = new JMenuItem(new AbstractAction("Open") {
+			@Override
+			public void actionPerformed(final ActionEvent the_event) {
+				JFileChooser fc = new JFileChooser();
+				int returnVal = fc.showOpenDialog(SpreadSheetGui.this);
+				if (returnVal == JFileChooser.APPROVE_OPTION)
+					try {
+						my_board.load(fc.getSelectedFile().getCanonicalPath());
+					} catch (Exception e) {
+					}
+				my_board.update();
+			}
+		});
+		open.setMnemonic('O');
+		file_menu.add(open);
+		final JMenuItem save = new JMenuItem(new AbstractAction("Save") {
+			@Override
+			public void actionPerformed(final ActionEvent the_event) {
+				JFileChooser fc = new JFileChooser();
+				int returnVal = fc.showSaveDialog(SpreadSheetGui.this);
+				if (returnVal == JFileChooser.APPROVE_OPTION)
+					try {
+						my_board.save(fc.getSelectedFile().getCanonicalPath());
+					} catch (Exception e) { }
+				my_board.update();
+			}
+		});
+		save.setMnemonic('S');
+		file_menu.add(save);
 		file_menu.addSeparator();
 		final Action close = new AbstractAction("Quit") {
 			public void actionPerformed(final ActionEvent the_event) {
@@ -93,20 +120,14 @@ public class SpreadSheetGui extends JFrame {
 		return file_menu;
 	}
 
-	@SuppressWarnings("unused")
-	private JMenu editMenu() {
-		final JMenu edit_menu = new JMenu("Edit");
-		edit_menu.setMnemonic('E');
-		return edit_menu;
-	}
-
 	private JMenu helpMenu() {
 		final JMenu help_menu = new JMenu("Help");
 		help_menu.setMnemonic('H');
 		final StringBuffer sb = new StringBuffer();
 		sb.append("Authors: \n");
 		sb.append("Miles Raymond - Teddy Doll - Son Pham\n");
-		sb.append("Documentation: http://code.google.com/p/spreadsheet-mst/wiki/HowToUseTheSpreadsheet");
+		sb
+				.append("Documentation: http://code.google.com/p/spreadsheet-mst/wiki/HowToUseTheSpreadsheet");
 		final Action about = new AbstractAction("About...") {
 			public void actionPerformed(final ActionEvent the_event) {
 				JOptionPane.showMessageDialog(null, sb,
@@ -120,14 +141,23 @@ public class SpreadSheetGui extends JFrame {
 	}
 
 	/**
-	 * The main method of SpreadSheetGui.
-	 * 
-	 * @param the_args
-	 *            The command line argument (ignored).
+	 * @return The menu bar.
 	 */
-	public static void main(final String... the_args) {
-		final SpreadSheetGui the_frame = new SpreadSheetGui(new Spreadsheet(
-				NUM_ROWS, NUM_COLS));
-		the_frame.setUp();
+	private JMenuBar menuBar() {
+		final JMenuBar menu_bar = new JMenuBar();
+		menu_bar.add(fileMenu());
+		// menu_bar.add(editMenu());
+		menu_bar.add(helpMenu());
+		return menu_bar;
+	}
+
+	/**
+	 * Setup the GUI.
+	 */
+	public void setUp() {
+		add(my_board);
+		setJMenuBar(menuBar());
+		pack();
+		setVisible(true);
 	}
 }
