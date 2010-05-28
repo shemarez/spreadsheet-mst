5c61d3e685e5f26811fa10aed44dcce0b4e44328
parabolster <parabolster@e0a764ce-2368-11df-b59c-8978f251a94e>
2010-05-28 06:18:27 +0000

    Deleted the comment for TCSS 390C.

--- spreadsheet/src/spreadsheet/Spreadsheet.java
+++ spreadsheet/src/spreadsheet/Spreadsheet.java
@@ -11,8 +11,7 @@ import tokens.CellToken;
  * @author Teddy Doll.
  * @version 1.0.
  */
-public class Spreadsheet
-{
+public class Spreadsheet {
 	// Instance fields
 
 	/**
@@ -36,10 +35,9 @@ public class Spreadsheet
 	 * Construct a spreadsheet with rows and columns is equal to size.
 	 * 
 	 * @param size
-	 *          The size.
+	 *            The size.
 	 */
-	public Spreadsheet(int size)
-	{
+	public Spreadsheet(int size) {
 		this.rows = size;
 		this.columns = size;
 		cellData = new Cell[rows][columns];
@@ -50,12 +48,11 @@ public class Spreadsheet
 	 * Construct a spreadsheet with rows and columns as the inputs.
 	 * 
 	 * @param rows
-	 *          The number of rows.
+	 *            The number of rows.
 	 * @param columns
-	 *          The number of columns.
+	 *            The number of columns.
 	 */
-	public Spreadsheet(int rows, int columns)
-	{
+	public Spreadsheet(int rows, int columns) {
 		this.rows = rows;
 		this.columns = columns;
 		cellData = new Cell[rows][columns];
@@ -67,12 +64,9 @@ public class Spreadsheet
 	/**
 	 * Filling 2-dimensional cell array with empty cells.
 	 */
-	private void populateCells()
-	{
-		for (int row = 0; row < this.rows; row++)
-		{
-			for (int col = 0; col < this.columns; col++)
-			{
+	private void populateCells() {
+		for (int row = 0; row < this.rows; row++) {
+			for (int col = 0; col < this.columns; col++) {
 				cellData[row][col] = new Cell();
 			}
 		}
@@ -82,19 +76,15 @@ public class Spreadsheet
 	 * Returns the cell at the row and column specified, of this spreadsheet.
 	 * 
 	 * @param row
-	 *          The row of the cell.
+	 *            The row of the cell.
 	 * @param column
-	 *          The column of the cell.
+	 *            The column of the cell.
 	 * @return A cell at row and column.
 	 */
-	public Cell getCell(int row, int column)
-	{
-		try
-		{
+	public Cell getCell(int row, int column) {
+		try {
 			return cellData[row][column];
-		}
-		catch (ArrayIndexOutOfBoundsException e)
-		{
+		} catch (ArrayIndexOutOfBoundsException e) {
 			throw e;
 		}
 	}
@@ -103,15 +93,15 @@ public class Spreadsheet
 	 * Update a cell formula and recalculate Spreadsheet.
 	 * 
 	 * @param cellToken
-	 *          The cell to update.
+	 *            The cell to update.
 	 * @param inputFormula
-	 *          The new formula for cell.
+	 *            The new formula for cell.
 	 * @throws CycleFound
 	 * @throws UnpairedParenthesesException
 	 */
 	public void changeCellFormulaAndRecalculate(CellToken cellToken,
-			String inputFormula) throws CycleFound, UnpairedParenthesesException
-	{
+			String inputFormula) throws CycleFound,
+			UnpairedParenthesesException {
 		cellData[cellToken.getRow()][cellToken.getColumn()].setFormula(
 				inputFormula, this);
 
@@ -124,27 +114,21 @@ public class Spreadsheet
 	 * found.
 	 * 
 	 * @param cellToken
-	 *          the cell to revert.
+	 *            the cell to revert.
 	 */
-	public void revert(CellToken cellToken)
-	{
-		try
-		{
+	public void revert(CellToken cellToken) {
+		try {
 			cellData[cellToken.getRow()][cellToken.getColumn()].revert(this);
-		}
-		catch (UnpairedParenthesesException e1)
-		{
+		} catch (UnpairedParenthesesException e1) {
 
 			System.err
 					.println("Parentheses error in reverting (Should never Happen!");
 		}
-		try
-		{
+		try {
 			topSort();
-		}
-		catch (CycleFound e)
-		{
-			System.err.println("Cycle found in reverting (Should never Happen!");
+		} catch (CycleFound e) {
+			System.err
+					.println("Cycle found in reverting (Should never Happen!");
 		}
 	}
 
@@ -153,69 +137,39 @@ public class Spreadsheet
 	 * 
 	 * @throws CycleFound
 	 */
-	private void topSort() throws CycleFound
-	{
-		// Some useful note for TCSS390C
-		/*
-		//Here's how you can get the indegree number of a cell.
-		Cell aCell = new Cell();
-		int numIndegree = aCell.indegree;
-		
-		//Here's how you evaluate a cell in this spreadsheet
-		aCell.evaluate(this);
-		
-		//Here's how you can get a list of cells that depend on aCell
-		LinkedList dependents = aCell.getDependents();
-		
-		//Here's how you can get an iterator that points to the front of the list (the first
-		//real node)
-		LinkedList.Iterator iterator = dependents.iterator();
-		
-		//Here's how you can get an iterator that points to the header node of a linked list
-		LinkedList.Iterator zeroth = dependents.zeroth();*/
-		
+	private void topSort() throws CycleFound {
 		int numVertices = this.rows * this.columns;
 		QueueAr q = new QueueAr(numVertices);
 		int counter = 0;
 		Cell cellV, cellW;
-		try
-		{
-			for (int row = 0; row < this.rows; row++)
-			{
-				for (int col = 0; col < this.columns; col++)
-				{
+		try {
+			for (int row = 0; row < this.rows; row++) {
+				for (int col = 0; col < this.columns; col++) {
 					cellV = cellData[row][col];
 					// reset temporary indegree to original indegree.
 					cellV.indegreeTemp = cellV.indegree;
-					if (cellV.indegreeTemp == 0)
-					{
+					if (cellV.indegreeTemp == 0) {
 						q.enqueue(cellV);
 					}
 				}
 			}
 
-			while (!q.isEmpty())
-			{
+			while (!q.isEmpty()) {
 				cellV = (Cell) q.dequeue();
 				cellV.evaluate(this);
 				counter++;
 				LinkedList.Iterator iter = cellV.dependents.iterator();
-				while (iter.hasNext())
-				{
+				while (iter.hasNext()) {
 					cellW = (Cell) iter.next();
-					if (--cellW.indegreeTemp == 0)
-					{
+					if (--cellW.indegreeTemp == 0) {
 						q.enqueue(cellW);
 					}
 				}
 			}
-			if (counter != numVertices)
-			{
+			if (counter != numVertices) {
 				throw new CycleFound();
 			}
-		}
-		catch (Overflow e)
-		{
+		} catch (Overflow e) {
 			System.out.println("Unexpected overflow");
 		}
 	}
@@ -224,26 +178,22 @@ public class Spreadsheet
 	 * Returns the value of the requested cell.
 	 * 
 	 * @param cellToken
-	 *          The cell for which value is requested.
+	 *            The cell for which value is requested.
 	 * @return The cell value.
 	 */
-	public int getCellValue(CellToken cellToken)
-	{
+	public int getCellValue(CellToken cellToken) {
 		return cellData[cellToken.getRow()][cellToken.getColumn()].getValue();
 	}
 
 	/**
 	 * Prints all the cells of the spreadsheet to the console.
 	 */
-	public void printValues()
-	{
+	public void printValues() {
 		StringBuffer sb = new StringBuffer();
 		sb.append("Spreadsheet values: \n");
 
-		for (int row = 0; row < this.rows; row++)
-		{
-			for (int col = 0; col < this.columns; col++)
-			{
+		for (int row = 0; row < this.rows; row++) {
+			for (int col = 0; col < this.columns; col++) {
 				String nextValue = cellData[row][col].printValue();
 				sb.append(nextValue);
 				// for proper spacing
@@ -260,12 +210,9 @@ public class Spreadsheet
 	/**
 	 * Print all the formulas in this spreadsheet.
 	 */
-	public void printAllFormulas()
-	{
-		for (int row = 0; row < this.rows; row++)
-		{
-			for (int col = 0; col < this.columns; col++)
-			{
+	public void printAllFormulas() {
+		for (int row = 0; row < this.rows; row++) {
+			for (int col = 0; col < this.columns; col++) {
 				System.out.println(cellData[row][col].printFormula());
 			}
 		}
@@ -275,10 +222,9 @@ public class Spreadsheet
 	 * Print the cell formula.
 	 * 
 	 * @param cellToken
-	 *          The cell token.
+	 *            The cell token.
 	 */
-	public void printCellFormula(CellToken cellToken)
-	{
+	public void printCellFormula(CellToken cellToken) {
 		System.out.println(cellData[cellToken.getRow()][cellToken.getColumn()]
 				.printFormula());
 	}
@@ -286,22 +232,21 @@ public class Spreadsheet
 	/**
 	 * 
 	 * @param cellToken
-	 *          The cell token.
+	 *            The cell token.
 	 * @return The cell formula.
 	 */
-	public String cellFormulaToString(CellToken cellToken)
-	{
-		return cellData[cellToken.getRow()][cellToken.getColumn()].printFormula();
+	public String cellFormulaToString(CellToken cellToken) {
+		return cellData[cellToken.getRow()][cellToken.getColumn()]
+				.printFormula();
 	}
 
 	/**
 	 * 
 	 * @param cellToken
-	 *          The cell token.
+	 *            The cell token.
 	 * @return The string representation of the cell's value.
 	 */
-	public String cellValueToString(CellToken cellToken)
-	{
+	public String cellValueToString(CellToken cellToken) {
 		return cellData[cellToken.getRow()][cellToken.getColumn()].printValue();
 	}
 
@@ -309,8 +254,7 @@ public class Spreadsheet
 	 * 
 	 * @return The number of rows.
 	 */
-	public int getNumRows()
-	{
+	public int getNumRows() {
 		return this.rows;
 	}
 
@@ -318,8 +262,7 @@ public class Spreadsheet
 	 * 
 	 * @return The number of columns.
 	 */
-	public int getNumColumns()
-	{
+	public int getNumColumns() {
 		return this.columns;
 	}
 
