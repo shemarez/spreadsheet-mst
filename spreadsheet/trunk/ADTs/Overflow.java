3e6b1fb1860606913a7635c9b3736172370314f7
parabolster <parabolster@e0a764ce-2368-11df-b59c-8978f251a94e>
2010-06-10 18:29:41 +0000

    Copy spreadsheet project to "trunk" folder.

---
+++ spreadsheet/trunk/ADTs/Overflow.java
@@ -0,0 +1,11 @@
+package ADTs;
+
+/**
+ * Exception class for access in full containers such as stacks, queues, and
+ * priority queues.
+ * 
+ * @author Mark Allen Weiss
+ */
+@SuppressWarnings("serial")
+public class Overflow extends Exception {
+}
